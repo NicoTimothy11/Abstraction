5e2bb2d79ed2ad535a0b09f54e811faef612c10d
NicoTimothy11 <nico.timothy319@gmail.com>
2023-08-25 10:57:48 +0800

    New Changes

--- DrawShape.cpp
+++ DrawShape.cpp
@@ -1,4 +1,28 @@
 #include <iostream>
 #include <string>
 
-class Shape {}
+using namespace std;
+
+class Base {
+public:
+	virtual void show() {
+		cout << "Virtual func" << endl;
+	}
+};
+
+class Derived : public Base {
+public:
+	void show() override {
+		cout << "Derived class function" << endl;
+	}
+};
+
+int main() {
+	Base *basePtr;
+	Derived derObj;
+
+	basePtr = &derObj;
+
+	basePtr -> show();
+	return 0;
+}
